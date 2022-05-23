a11b514b8ace33015f2e3c11af0dd6ecbdeea68d
Pawel_Lewicki <pawlew2@st.wmi.amu.edu.pl>
2022-05-23 12:25:55 +0200

    REFACTOR additional parameters in function min_mse

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def min_mse(values):
-
+def min_mse(values, quant_nr, epsilon=0.1, max_quanting_range=2):
     mse_min_all = 100
     mse_min_value = 0
 
-    for delta in np.arange(0, 2, 0.1):
-        quants = [delta * k for k in range(-5, 5)]
+    for delta in np.arange(0, max_quanting_range, epsilon):
         mse_total = 0
+
+        quants = [delta * k for k in range(-quant_nr, quant_nr)]
 
         # Licz MSE dla wartości
         for i in values:
@@ -34,9 +34,12 @@ def min_mse(values):
 if __name__ == '__main__':
     # USER INPUTS
     input_values = [1.1, 3.2, -5.5, 4.2, -5.5, 0, -0.7]
+    quant_number = 10
 
     # FUNCTION
-    result_index, result_value = min_mse(input_values)
+    quant_number = int(quant_number / 2)
+
+    result_index, result_value = min_mse(input_values, quant_number)
 
     # RESULT
     print("\n### RESULTS ###\n")
